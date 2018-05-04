ae1aa2879254bc17568ce1629a4bfb651ac3e3e6
ashbakernz <ashbakernz@gmail.com>
2018-05-04 21:02:34 +1200

    Added employee table/migration/relation, Added spatie/laravel-fractal package

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Employee;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        $employees = Employee::latestFirst()->paginate(7);
+    }
+}
