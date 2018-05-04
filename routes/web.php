87720a4630abb336751780246b3db2da9faa1d16
ashbakernz <ashbakernz@gmail.com>
2018-05-05 01:11:43 +1200

    Added departments controller, migration and route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/employees ', 'EmployeeController@index');
+Route::get('/departments ', 'DepartmentController@index');
